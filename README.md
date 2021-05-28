6118eec17ae98ebae132f8d3d2a66814ffb6910b
HazemMeqdad <hazemmeqdad@gmail.com>
2021-05-28 18:53:08 +0300

    ⭐ Source code

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # anti-ahlin-chan
+
+---
+
+## idea
+
+My friend is following me on the `github.com` site,
+I told him that if you delete the following from me I will cancel your following, 
+for this reason I created the project
+
+---
+
+## thanks
+
+Thanks to my friend for giving me the idea
+https://github.com/al-bimani
+
+---
+
+## Copyright
+
+```commandline
+Copyright (c) 2021 HazemMeqdad
+```
+---
---
+++ main.py
@@ -0,0 +1,59 @@
+"""
+MIT License
+
+Copyright (c) 2021 HazemMeqdad
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import requests
+import bs4
+import discord
+from discord.ext import tasks
+import os
+
+bot = discord.Client()  # bot client
+
+my_page_followers = "https://github.com/HazemMeqdad?tab=followers"  # page Ahlin Chan
+required_account = "al-bimani"  # name Ahlin Chan
+
+webhook = os.environ['webhook']
+token = os.environ['token']
+
+
+@tasks.loop(minutes=1)
+async def show_Ahlin_Chan():
+    date = requests.get(my_page_followers)
+    soup = bs4.BeautifulSoup(date.content, "lxml")
+    followers = soup.find_all("span", {"class": "Link--secondary"})
+    list_followers = [x.text for x in followers]  # followers list
+
+    if required_account not in list_followers:
+        requests.post(webhook, json={"content": "الحق اهلين شان شال عنك الفلوا بسرعا <@750376850768789534>"})
+        print("Fuck")
+
+
+@bot.event
+async def on_ready():
+    print("Hazem show `Ahlin Chan` now")
+    show_Ahlin_Chan.start()
+
+
+bot.run(token)
+
